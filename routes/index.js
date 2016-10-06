0d780dc2ee954af9fad0786b155119c0499ea452
kiraly96 <kiraly996@gmail.com>
2016-10-06 13:24:45 +0200

    Postgre connect

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var pg = require('pg');	
+var connectionString = process.env.DATABASE_URL || 'postgres://felhnev:jelszo@localhost:5432/tanulo';	
+
+
+/* GET HOMEPAGE */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', {
-    title: 'Tanuló SCH',
-    profile_name: 'Király Bálint'
-  });
+
+    var results = [];	
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) { 
+
+       
+        if(err) {
+          done();
+          console.log(err);	
+          return res.status(500).json({ success: false, data: err}); 
+        }
+
+        // SQL Query
+        var query = client.query("SELECT * FROM users ORDER BY user_ID ASC;");
+
+
+        query.on('row', function(row) {
+            results.push(row); //a sor tartalmat push-oljuk a results tombbe
+        });
+
+      
+        query.on('end', function() {
+          done(res.render('pages/index', {data: results}) );	
+          return results;
+        });
+
+    });
+
 });
+
 
 module.exports = router;
