5f4d1d1ac7287e3ddab843e9b00595cd2b09f5ec
kiraly96 <kiraly996@gmail.com>
2016-10-30 21:22:06 +0100

    DB column name fix

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-var pg = require('pg');	
-var connectionString = process.env.DATABASE_URL || 'postgres://felhnev:jelszo@localhost:5432/tanulo';	
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://felhnev:jelszo@localhost:5432/tanulo';
 
 
 /* GET HOMEPAGE */
 router.get('/', function(req, res, next) {
 
-    var results = [];	
+    var results = [];
 
     // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) { 
+    pg.connect(connectionString, function(err, client, done) {
 
-       
+
         if(err) {
           done();
-          console.log(err);	
-          return res.status(500).json({ success: false, data: err}); 
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
         }
 
         // SQL Query
@@ -28,9 +28,9 @@ router.get('/', function(req, res, next) {
             results.push(row); //a sor tartalmat push-oljuk a results tombbe
         });
 
-      
+
         query.on('end', function() {
-          done(res.render('pages/index', {data: results}) );	
+          done(res.render('pages/index', {data: results}) );
           return results;
         });
 
