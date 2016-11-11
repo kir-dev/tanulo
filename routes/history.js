ddcf4597847ffcff419b7574837595e121331de1
Fazekas Zsolt <fazekas.zsolt95@gmail.com>
2016-11-11 21:06:35 +0100

    Unfinished implementation of history

--- routes/history.js
+++ routes/history.js
@@ -12,14 +12,19 @@ router.get('/', function(req, res, next) {
     if (!req.isAuthenticated()) { res.redirect('/'); }
      pg.connect(connectionString, function(err, client, done) {
 
-        if(err) {
+        if(err) 
+        {
           done();
           console.log(err);
           return res.status(500).json({ success: false, data: err});
         }
 
         var results = [];
-        var query = client.query("SELECT group_name, group_description, group_start_date, group_end_date, subject_name FROM db_group INNER JOIN db_group_user ON db_group.group_id = db_group_user.group_id INNER JOIN db_subject on db_group.subject_id = db_subject.subject_id");
+        var query = client.query("SELECT group_name, group_description, group_start_date, group_end_date, subject_name " 
+        	+ "FROM db_group INNER JOIN db_group_user ON db_group.group_id = db_group_user.group_id "
+        	+ "INNER JOIN db_subject on db_group.subject_id = db_subject.subject_id"
+        	+ "WHERE db_user.user_authsch_id = $1"
+        	,[req.user.internal_id]);
 
 
         query.on('row', function(row) {
