4b252156bb0e754bfb3c7f742311aae5c9ed2f7d
kiralybalint <kiraly.balint@simonyi.bme.hu>
2017-09-26 10:27:24 +0200

    Fix database connaction parameters

--- models/index.js
+++ models/index.js
@@ -7,7 +7,11 @@ var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var db        = {};
 
-var sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, process.env);
+var sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, 
+{
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
 
 fs
   .readdirSync(__dirname)
