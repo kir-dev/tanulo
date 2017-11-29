86a8be2bf4558504fe1fe4763910c22d049cbb6d
fenyopeti <petyu8@msn.com>
2017-11-29 01:29:58 +0100

    Bower init

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
