1431e90986e630688b1078b65b9e69b413e69aff
Zsolt Fazekas <fazekas.zsolt95@gmail.com>
2017-11-30 20:42:08 +0100

    Cannot reserve room if there is a time conflict with another event.

--- middleware/group/addGroup.js
+++ middleware/group/addGroup.js
@@ -1,7 +1,39 @@
 var addGroup = function (models) {
-    var createGroup = function (groupDTO) {
+    var createGroup = function (groupDTO, res, next) {
+
+        //Check conflicts
+        models.group.findOne({
+            where: {
+                $or: [
+                    {
+                        $and: [
+                            {
+                                startDate: {lt: groupDTO.end_date}
+                            },
+                            {
+                                endDate: {gt: groupDTO.end_date}
+                            }
+                        ]
+                    },
+                    {
+                        $and: [
+                            {
+                                startDate: {lt: groupDTO.start_date}
+                            },
+                            {
+                                endDate: {gt: groupDTO.start_date}
+                            }]
+                    }]
+            }
+
+        }).then(function (event) {
+            if(event !== null){
+                res.status('401').send({error:' A megadott idopontban mar foglalt a tanulo'});
+            }
+        });
+
         let dnd = false;
-        if(groupDTO.doNotDisturb){
+        if (groupDTO.doNotDisturb) {
             dnd = true;
         }
         return models.group.create({
@@ -16,11 +48,8 @@ var addGroup = function (models) {
     };
 
     return function (req, res, next) {
-        createGroup(req.body).then(function (err) {
-            if(err){
-                //TODO error handling
-                //console.log(err);    
-            }
+        createGroup(req.body, res, next).then(function (err) {
+
             return next();
         });
     };
